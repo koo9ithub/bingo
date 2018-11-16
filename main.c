83f521448bde492331748422d50a511c8df96895
koo9ithub <kk223704@naver.com>
2018-11-16 14:17:18 +0900

    3rd submission

--- main.c
+++ main.c
@@ -1,50 +1,88 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
-#define N		5	//size of bingo
+#define N		5	//size of bingo (N*N)
 #define M		3	//line that is a requirement for winning
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int initiate_bingo()
-{
-}
 
-int print_bingo()
-{
-}
 
-int get_num_byMe()
+int initiate_bingo(table[][N])
 {
 }
+/* make a initial bingotable. */
 
-int get_num_byCom()
+int print_bingo(table[][N])
 {
 }
+/* print a present bingotable. */
+
+int get_num_byMe(int Mychoice)
+{
+}
+/* select my choice by hand. */ 
+
+int get_num_byCom(int Comchoice)
+{
+}
+/* select Com's choice by random. */
+
+int process_bingo()
+{
+}
+/* recieve my/Com's choice, fill a block of bingotable. */
+
+int count_bingo()
+{
+}
+/* count lines that are filled. */ 
 
 
 
 int main(int argc, char *argv[]) {
 	
+	/* declare variables. */
+	
 	int i;
-	int BingoTable1[N]; //make one bingotable.
-	int BingoTable2[N]; //make the other bingotable.
+	int num_turn;
+	char winner;
 	
-	initiate_bingo();
-	/* make a bingotable by random */
 	
-	print_bingo();
-	/* show a bingotable that made by random */
 	
-	for (i=0; i<N; i++)
+	/* initial preperation */
+
+	initiate_bingo(Mybt[][N]);
+	initiate_bingo(Combt[][N]);
+	
+	print_bingo(Mybt[][N]);
+	
+	
+	
+	/* game process */
+	
+	i=count_bingo();
+	
+	for (i=0; i<M; i++)
 	{
 		get_num_byMe();
+		process_bingo();
+		
 		get_num_byCom();
+		process_bingo();
 	}
+	
+	
+	
+	/* ending */
+	
+	winner
+	printf("Winner: %c", winner);
+	
+	num_turn
+	printf("How many turns? %i turns.", num_turn);
 
-	
-	
-	
 	
 	return 0;
 }
