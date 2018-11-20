be2b476aa37bf2a19526142b443adbd38713eafa
koo9ithub <kk223704@naver.com>
2018-11-20 09:29:16 +0900

    final_1st submission

--- main3.c
+++ main3.c
@@ -10,86 +10,28 @@
 
 
 
-/* 1. initiate_bingo: 빙고 테이블을 초기에 만들어줌. */
-void initiate_bingo(){ 
-
-	int Bingotable[N][N]={{0},{0},{0},{0},{0}}; //bingo table 초기화
-	
-	int overlap[N*N] = {0}; //중복 난수를 확인하기 위한 배열
-	int top = 0;
-	int i, j, k; 
-	 
-	
-	srand((unsigned)time(NULL));
-	
-	while (top < N*N){
-		
-		overlap[top] = 1 + rand()%(N*N);
-		
-		for (i=0; i<top; i++){
-			
-			if (overlap[top] == overlap[i]) {
-				top--;
-			}
-		}
-		top++;
-	}
-	top = 0; //중복 난수가 없음을 체크 후 빙고판에 작성. 
-
-	for (j=0; j<N; j++){
-		for (k=0; k<N; k++){
-			
-			Bingotable[j][k] = overlap[top];
-			top++;
-		}
-	}
-	
-	char check [N][N] = {
-	{'X', 'X', 'X', 'X', 'X'},
-	{'X', 'X', 'X', 'X', 'X'},
-	{'X', 'X', 'X', 'X', 'X'},
-	{'X', 'X', 'X', 'X', 'X'},
-	{'X', 'X', 'X', 'X', 'X'}
-	} ; //확인 
-}
-
-
-/* 2. print_bingo: 빙고 테이블 현재 상황을 화면에 출력 */
-void print_bingo(int Bingotable[][N], char check[][N]){
-	
-	int j, k;
-	char left0 = '(';
-	char right0 = ')';
-	
-	for (j=0; j<N; j++){
-		for (k=0; k<N; k++){
-			
-			if (check[j][k] == '0'){
-				
-				printf("%c%2d%c", left0, Bingotable[j][k], right0);
-			}
-			else{
-				printf("%2d", Bingotable[j][k]);
-			}
-		}
-		printf("\n");
-	}
-	printf("\n");
-}
-
-
+/* 1. 필요한 함수들을 선언. */
+void initiate_bingo(int *);
+void print_bingo();
+int get_number_byMe();
+int get_number_byCom();
+void process_bingo();
+int count_bingo();
 		
  
-
+/* 2. 메인 함수 */
 int main(int argc, char *argv[]) {
 	
-	int Mybt[N][N];
-	char check[N][N];
+	while(1) 
+	{
+		int Mybt[N][N];
+		int Combt[N][N]; 
+		
+		printf("***** BINGO GAME with your computer ******\n");
 	
-	printf("***** BINGO GAME with your computer ******\n");
+		initiate_bingo();
+		print_bingo(Mybt);
 	
-	initiate_bingo();
-	print_bingo(Mybt, check);
-	
+	}
 	return 0;
 }
